48e20a95da559ec0e2051ceab8b95d1a2183eb10
Shafayat <mindfreakicup33@gmail.com>
2019-08-20 20:27:20 +0600

    Retrofit base url changed and added converters.

---
+++ app/src/main/java/com/gigatech/ekyc/remote/RetroFitInstance.java
@@ -0,0 +1,28 @@
+package com.gigatech.ekyc.remote;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroFitInstance {
+
+    private static Retrofit retrofit;
+    private static String BASE_URL = "http://puthi-ocr.com:3000/app/";
+
+    public static Retrofit retrofitInstance() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+
+        return retrofit;
+
+    }
+
+}
