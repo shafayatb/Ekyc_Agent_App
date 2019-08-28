ee11bbb11536f5c6b6ec9ffaafb8b040cc500588
Shafayat <mindfreakicup33@gmail.com>
2019-08-28 11:18:51 +0600

    Added buttons in success msg activity

--- app/src/main/java/com/gigatech/ekyc/SuccessNotification.java
+++ app/src/main/java/com/gigatech/ekyc/SuccessNotification.java
@@ -1,13 +1,29 @@
 package com.gigatech.ekyc;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
 public class SuccessNotification extends AppCompatActivity {
+
+    AppCompatButton goToNewUserRegistration, goToHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_notification);
+
+        goToHome = findViewById(R.id.goToHome);
+        goToNewUserRegistration = findViewById(R.id.goToNewUserRegistration);
+
+        goToHome.setOnClickListener(v -> {
+            startActivity(new Intent(this, HomeActivity.class));
+            finishAffinity();
+        });
+
+        goToNewUserRegistration.setOnClickListener(v -> startActivity(new Intent(this, NidFrontSideCapture.class)));
+
     }
 }
